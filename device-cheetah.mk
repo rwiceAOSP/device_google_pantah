6ca8f23efd0722556c971d5da32ebf8264fb9e08
Michael Bestas <mkbestas@lineageos.org>
2026-03-16 12:17:39 +0200

    pantah: Move remaining lineage overlays to gs201
    These are common between all non tablet gs201 devices.
    
    Change-Id: I74ced1edef9086aa2b87a14832a7bd91ca765fbe

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -15,9 +15,6 @@ TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 include device/google/gs201/device-shipping-common.mk
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    device/google/pantah/overlay-lineage
-
 PRODUCT_PACKAGES += \
     FrameworkResOverlayProductPantah \
     FrameworkResOverlayVendorPantah \
